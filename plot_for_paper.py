02a8bbee75e16155c45344e2e927a807c4bb401a
liuyishoua <1813120647@qq.com>
2022-11-24 00:15:18 +0800

    11/24

--- plot_for_paper.py
+++ plot_for_paper.py
@@ -1,3 +1,5 @@
+import matplotlib
+import matplotlib as mpl
 import numpy as np
 from utils import read_param2per
 import matplotlib.pyplot as plt
@@ -40,9 +42,12 @@ def heatmap(data, row_labels, col_labels, ax=None,
     cbar.ax.set_ylabel(cbarlabel, rotation=-90, va="bottom")
 
     # Show all ticks and label them with the respective list entries.
-    ax.set_xticks(np.arange(data.shape[1]), labels=col_labels)
-    ax.set_yticks(np.arange(data.shape[0]), labels=row_labels)
-
+    print (data.shape[1])
+    print (col_labels)
+    # ax.set_xticks(np.arange(data.shape[1]), labels=col_labels)
+    ax.set_xticks(np.arange(data.shape[1]))
+    # ax.set_yticks(np.arange(data.shape[0]), labels=row_labels)
+    ax.set_yticks(np.arange(data.shape[0]))
     # Let the horizontal axes labeling appear on top.
     ax.tick_params(top=True, bottom=False,
                    labeltop=True, labelbottom=False)
@@ -52,7 +57,7 @@ def heatmap(data, row_labels, col_labels, ax=None,
              rotation_mode="anchor")
 
     # Turn spines off and create white grid.
-    ax.spines[:].set_visible(False)
+    # ax.spines[:].set_visible(False)
 
     ax.set_xticks(np.arange(data.shape[1]+1)-.5, minor=True)
     ax.set_yticks(np.arange(data.shape[0]+1)-.5, minor=True)
@@ -115,7 +120,7 @@ def annotate_heatmap(im, data=None, valfmt="{x:.2f}",
     for i in range(data.shape[0]):
         for j in range(data.shape[1]):
             kw.update(color=textcolors[int(im.norm(data[i, j]) > threshold)])
-            text = im.axes.text(j, i, valfmt(data[i, j], None), **kw)
+            text = im.axes.text(j, i, round(data[i, j],2), None, **kw)
             texts.append(text)
 
     return texts
@@ -128,7 +133,7 @@ if __name__ == "__main__":
     param2per = read_param2per("./result/param2performance_weibo_min_gatbw.txt")
     data = param2per[:,2].reshape(11,10)
     # np.random.seed(19680801)
-    fig, ((ax, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(8, 6))
+    fig, (ax2) = plt.subplots(1, 1, figsize=(8, 6))
 
     # Replicate the above example with a different font size and colormap.
 
@@ -140,49 +145,48 @@ if __name__ == "__main__":
     # use an integer format on the annotations and provide some colors.
 
     # data = np.random.randint(2, 100, size=(7, 7))
-    y = ["Book {}".format(i) for i in range(1, 10)]
-    x = ["Store {}".format(i) for i in list("ABCDEFGHIJK")]
-    im, _ = heatmap(data, y, x, ax=ax2, vmin=0,
+    y = ["Book {}".format(i) for i in range(1, 11)]
+    x = ["Store {}".format(i) for i in list("ABCDEFGHIJ")]
+    im, _ = heatmap(data, y, x, ax=ax2, vmin=0.85,
                     cmap="magma_r", cbarlabel="weekly sold copies")
-    annotate_heatmap(im, valfmt="{x:d}", size=10, threshold=20,
-                    textcolors=("red", "white"))
+    annotate_heatmap(im, valfmt="{x:d}", size=10, threshold=20, textcolors=("red", "white"))
 
     # Sometimes even the data itself is categorical. Here we use a
     # `matplotlib.colors.BoundaryNorm` to get the data into classes
     # and use this to colorize the plot, but also to obtain the class
     # labels from an array of classes.
 
-    data = np.random.randn(6, 6)
-    y = ["Prod. {}".format(i) for i in range(10, 70, 10)]
-    x = ["Cycle {}".format(i) for i in range(1, 7)]
+    # data = np.random.randn(6, 6)
+    # y = ["Prod. {}".format(i) for i in range(10, 70, 10)]
+    # x = ["Cycle {}".format(i) for i in range(1, 7)]
 
-    qrates = list("ABCDEFG")
-    norm = matplotlib.colors.BoundaryNorm(np.linspace(-3.5, 3.5, 8), 7)
-    fmt = matplotlib.ticker.FuncFormatter(lambda x, pos: qrates[::-1][norm(x)])
+    # qrates = list("ABCDEFG")
+    # norm = matplotlib.colors.BoundaryNorm(np.linspace(-3.5, 3.5, 8), 7)
+    # fmt = matplotlib.ticker.FuncFormatter(lambda x, pos: qrates[::-1][norm(x)])
 
-    im, _ = heatmap(data, y, x, ax=ax3,
-                    cmap=mpl.colormaps["PiYG"].resampled(7), norm=norm,
-                    cbar_kw=dict(ticks=np.arange(-3, 4), format=fmt),
-                    cbarlabel="Quality Rating")
+    # im, _ = heatmap(data, y, x, ax=ax3,
+    #                 cmap=mpl.colormaps["PiYG"].resampled(7), norm=norm,
+    #                 cbar_kw=dict(ticks=np.arange(-3, 4), format=fmt),
+    #                 cbarlabel="Quality Rating")
 
-    annotate_heatmap(im, valfmt=fmt, size=9, fontweight="bold", threshold=-1,
-                    textcolors=("red", "black"))
+    # annotate_heatmap(im, valfmt=fmt, size=9, fontweight="bold", threshold=-1,
+    #                 textcolors=("red", "black"))
 
     # We can nicely plot a correlation matrix. Since this is bound by -1 and 1,
     # we use those as vmin and vmax. We may also remove leading zeros and hide
     # the diagonal elements (which are all 1) by using a
     # `matplotlib.ticker.FuncFormatter`.
 
-    corr_matrix = np.corrcoef(harvest)
-    im, _ = heatmap(corr_matrix, vegetables, vegetables, ax=ax4,
-                    cmap="PuOr", vmin=-1, vmax=1,
-                    cbarlabel="correlation coeff.")
+    # corr_matrix = np.corrcoef(harvest)
+    # im, _ = heatmap(corr_matrix, vegetables, vegetables, ax=ax4,
+    #                 cmap="PuOr", vmin=-1, vmax=1,
+    #                 cbarlabel="correlation coeff.")
 
 
-    def func(x, pos):
-        return "{:.2f}".format(x).replace("0.", ".").replace("1.00", "")
+    # def func(x, pos):
+    #     return "{:.2f}".format(x).replace("0.", ".").replace("1.00", "")
 
-    annotate_heatmap(im, valfmt=matplotlib.ticker.FuncFormatter(func), size=7)
+    # annotate_heatmap(im, valfmt=matplotlib.ticker.FuncFormatter(func), size=7)
 
 
     plt.tight_layout()
